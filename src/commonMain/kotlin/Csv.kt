df77b9a113e67a94b43cead873475d02596d1838
Sergej Shafarenka <sergej.shafarenka@halfbit.de>
2024-09-16 23:53:01 +0200

    Stronger typing for header and data rows

--- src/commonMain/kotlin/Csv.kt
+++ src/commonMain/kotlin/Csv.kt
@@ -2,8 +2,26 @@
 package de.halfbit.csv
 
 public class Csv(
-    public val rows: List<List<String>>,
+    public val header: HeaderRow?,
+    public val data: List<DataRow>,
 ) {
+    public val rows: List<Row> by lazy {
+        buildList {
+            header?.let { add(it) }
+            addAll(data)
+        }
+    }
+
+    public sealed interface Row : List<String>
+
+    public interface HeaderRow : Row {
+        public fun indexOfColumn(name: String): Int
+    }
+
+    public interface DataRow : Row {
+        public fun value(columnName: String): String?
+    }
+
     // Multiline issue: https://stackoverflow.com/questions/2668678/importing-csv-with-line-breaks-in-excel-2007
     public fun toCsvText(
         newLine: NewLine = NewLine.LF,
@@ -20,6 +38,17 @@ public class Csv(
             append(newLine.value)
         }
     }
+
+    override fun toString(): String =
+        buildString {
+            append(header)
+            append("\n")
+            data.forEach { data ->
+                append(data)
+                append("\n")
+            }
+        }
+
 }
 
 public enum class NewLine(
